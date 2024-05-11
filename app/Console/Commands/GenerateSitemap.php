1b064035ae68cf189fc6d7bd6a63353af75250d6
Valery Zonov <valery.zonov@sibers.com>
2024-05-12 00:12:05 +0700

    first test command (success validation file extensions )

--- app/Console/Commands/GenerateSitemap.php
+++ app/Console/Commands/GenerateSitemap.php
@@ -3,6 +3,9 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use vakazona\SitemapGenerator\DTO\SitemapData;
+use vakazona\SitemapGenerator\SitemapGenerator;
+
 
 class GenerateSitemap extends Command
 {
@@ -25,6 +28,21 @@ class GenerateSitemap extends Command
      */
     public function handle()
     {
-        //
+        $pages = [
+            ['loc' => 'https://example.com/page1', 'lastmod' => '2024-05-11', 'priority' => 0.8, 'changefreq' => 'daily'],
+            ['loc' => 'https://example.com/page2', 'lastmod' => '2024-05-10', 'priority' => 0.5, 'changefreq' => 'weekly']
+        ];
+
+        $fileType = 'txt';
+        $filePath = storage_path('app/sitemap.csv');
+
+        $sitemapGenerator = new SitemapGenerator(new SitemapData([
+            'pages' => $pages,
+            'fileType' => $fileType,
+            'filePath' => $filePath
+        ]));
+        $sitemapGenerator->generateSitemap();
+
+        $this->info('Sitemap generated successfully.');
     }
 }
