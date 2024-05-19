28effb210dbe4cc17aecfeaccd6a31ee1386085a
Valery Zonov <valery.zonov@sibers.com>
2024-05-20 00:41:49 +0700

    new example

--- app/Console/Commands/GenerateSitemap.php
+++ app/Console/Commands/GenerateSitemap.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use vakazona\SitemapGenerator\DTO\SitemapData;
 use vakazona\SitemapGenerator\SitemapGenerator;
+use vakazona\SitemapGenerator\Validator\SitemapValidator;
 
 
 class GenerateSitemap extends Command
@@ -33,15 +34,16 @@ class GenerateSitemap extends Command
             ['loc' => 'https://example.com/page2', 'lastmod' => '2024-05-10', 'priority' => 0.5, 'changefreq' => 'weekly']
         ];
 
-        $fileType = 'xml';
-        $filePath = storage_path('/app/upload/sitemap.xml');
+        $fileType = 'json';
+        $filePath = storage_path('/app/upload/sitemap.json');
 
-        $sitemapGenerator = new SitemapGenerator(new SitemapData([
+
+        $sitemapGenerator = new SitemapGenerator(new SitemapValidator());
+        $sitemapGenerator->generateSitemap(new SitemapData([
             'pages' => $pages,
             'fileType' => $fileType,
-            'filePath' => $filePath
+            'filePath' => $filePath,
         ]));
-        $sitemapGenerator->generateSitemap();
 
         $this->info('Sitemap generated successfully.');
     }
